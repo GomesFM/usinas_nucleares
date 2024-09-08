604344f79300346f6057c1bdc6967c3d0bfd6ce2
GomesFM <fmgomes93@gmail.com>
2024-09-07 21:20:03 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ function pesquisar() {
 
   // Verifica se o campo de pesquisa está vazio
   if (!campoPesquisa) {
+
     // Exibe uma mensagem de erro caso o campo esteja vazio
-    section.innerHTML = "<p>Sem resultado! Por favor digite um país na busca.</p>"
+    section.innerHTML = "<p>Sem resultado! Por favor digite um país na busca.</p>";
     return; // Interrompe a função
-  }
+  };
 
   // Converte o termo de pesquisa para minúsculas para facilitar a comparação
   campoPesquisa = campoPesquisa.toLowerCase();
@@ -20,27 +21,36 @@ function pesquisar() {
 
   // Itera sobre cada país no array de países com usinas nucleares
   for (let pais of paisesUsinas) {
-    // Converte as tags do país para minúsculas para facilitar a comparação
-    tags = pais.tags.toLowerCase();
 
-    // Verifica se as tags do país incluem o termo de pesquisa
-    if (tags.includes(campoPesquisa)) {
-      // Constrói o HTML para cada resultado da pesquisa, formatando os dados do objeto
-      resultados += `
-        <div class="item-resultado">
-          <h2>${pais.pais}</h2>
-          <p class="descricao-meta">${pais.descricao}</p>
-          <a href=${pais.fonte} target="_blank"><br><br>Fonte dos dados<br><br></a>
-          <button onclick="reatores()">Para saber mais sobre os tipos de reatores</button>
-        </div>`;
-    }
-  }
+    // Converte as tags do país para minúsculas para facilitar a comparação
+    tagsPais = pais.tags.toLowerCase();
+
+    // Converte as tags do pais para uma lista
+    listaTags = tagsPais.split(",").map(tag => tag.trim())
+
+    // Itera sobre a lista de tags para encontrar a igualdade com o temo de pesauisa
+    for (let i = 0; i < listaTags.length; i++) {
+
+      // Verifica se as tags do país são iguais ao termo de pesquisa
+      if (listaTags[i] == campoPesquisa) {
+        
+        // Constrói o HTML para cada resultado da pesquisa, formatando os dados do objeto
+        resultados += `
+          <div class="item-resultado">
+            <h2>${pais.pais}</h2>
+            <p class="descricao-meta">${pais.descricao}</p>
+            <a href=${pais.fonte} target="_blank"><br>Fonte dos dados<br><br></a>
+            <button onclick="reatores()">Para saber mais sobre os tipos de reatores</button>
+          </div>`;
+      };
+    };
+  };
 
   // Verifica se foram encontrados resultados
   if (!resultados) {
     // Exibe uma mensagem indicando que não foram encontrados resultados
     resultados = "<p>Este país não possui usinas nucleares.</p>";
-  }
+  };
 
   // Atualiza o conteúdo da seção de resultados com o HTML gerado
   section.innerHTML = resultados;
